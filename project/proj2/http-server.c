9c892eafa7aebc33198db68fae557c14e51fa515
Daniel Kao <dkao@diplateevo.com>
2016-01-31 18:30:49 -0800

    add initial files

---
+++ project/proj2/http-server.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <inttypes.h>
+#include <ctype.h>
+#include <arpa/inet.h>
+#include "Practical.h"
+
+/**
+ * http-client.c
+ *
+ * Author: Daniel Kao
+ * PID: A10546439
+ * Date: 1/19/16
+ *
+ */
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+int main(int argc, char *argv[]) {
+
+    fputs("0", stdout);     // Print the echo buffer
+    return 0;
+}
